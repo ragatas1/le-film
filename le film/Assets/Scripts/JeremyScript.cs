5fd55e58c1645618973478243764e60f5f28b331
ragatas1 <emil.sonstaberg@gmail.com>
2024-11-12 10:48:23 +0100

    base movement for jeremy

---
+++ le film/Assets/Scripts/JeremyScript.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class JeremyScript : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] Rigidbody2D rb;
+    float playerDistance;
+    [SerializeField] float targetDistance;
+    [SerializeField] float ishRange;
+    [SerializeField] float moveSpeed;
+    int horizontalModifier;
+    int verticalDirection;
+    float horizontalDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(changeDirection());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerDistance = Vector2.Distance(player.position, transform.position);
+        if (playerDistance > targetDistance+ishRange) 
+        {
+            aproach();
+            verticalDirection = 1;
+            horizontalModifier = 1;
+        }
+        else if (playerDistance < targetDistance - ishRange)
+        {
+            aproach();
+            verticalDirection = -1;
+            horizontalModifier = 1;
+        }
+        else
+        {
+            horizontalModifier = 2;
+            around();
+        }
+        turn();
+    }
+    void turn()
+    {
+        var dir = player.position - transform.position;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+    }
+    void around()
+    {
+        rb.MovePosition(transform.position + transform.right * moveSpeed * horizontalDirection * Time.deltaTime);
+    }
+    void aproach()
+    {
+        rb.MovePosition(transform.position+transform.up*moveSpeed* verticalDirection * Time.deltaTime);
+    }
+    IEnumerator changeDirection()
+    {
+        horizontalDirection = Random.Range(-1, 1);
+        yield return new WaitForSeconds(2);
+        StartCoroutine(changeDirection());
+    }
+}
