0bc6865699132e8204e385457d7596060babc488
ragatas1 <emil.sonstaberg@gmail.com>
2024-11-18 10:00:52 +0100

    made a menu

---
+++ le film/Assets/Scripts/StartScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScript : MonoBehaviour
+{
+    [SerializeField] string scene;
+    void Update()
+    {
+        if (Input.GetButtonDown("roll"))
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
